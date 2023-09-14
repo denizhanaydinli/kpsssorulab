5e2d1278ad7adda3e5698c1a19491f116ea2877d
denizhanayinli <daydinli93@gmail.com>
2023-09-14 16:36:49 +0300

    hedefsoru ekranı düzenlendi rsim eklendi textfield alanı eklendi altındaki ilerle butonunun routingi daha yapılmadı

--- lib/hedefsoru.dart
+++ lib/hedefsoru.dart
@@ -8,8 +8,78 @@ class HedefScreen extends StatefulWidget {
 }
 
 class _HedefScreenState extends State<HedefScreen> {
+  void _onLoginButtonPressed() {
+    // Burada yapılacak işlem: Kullanıcı adı ve şifreyi kontrol edebilir ve giriş işlemi yapabilirsiniz.
+
+    // Giriş başarılı ise hedef ekranına yönlendirme
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HedefScreen()),//burası yeni ekrana yönlendirilecek
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFF302D2D),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    SizedBox(height: 100.0),
+                    Text(
+                      "Günlük kaç soru çözme bildirimi almak istersiniz?",
+                      style: TextStyle(
+                        color: Colors.white, // Yazı rengi beyaz
+                        fontWeight: FontWeight.bold, // Kalın yazı tipi
+                        fontSize: 20.0, // Yazı boyutu
+                      ),
+                    ),
+                    SizedBox(height: 100.0),
+                    Image.asset(
+                      "assets/hedefsoru.png",
+                      scale: 3.0,
+                      fit: BoxFit.scaleDown,
+                    ),
+                    SizedBox(height: 48.0),
+                // Resim ile metin arasında bir boşluk ekler
+                    Container(
+                      width: 600, // Genişlik
+                      padding: EdgeInsets.all(10.0), // İçeriği kenara doğru boşluk bırakır
+                      decoration: BoxDecoration(
+                        color: Colors.white, // Beyaz arka plan rengi
+                        borderRadius: BorderRadius.circular(10.0), // Kenarlığın yuvarlak köşeli olması
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Soru Hedefi',
+                          border: InputBorder.none, // Kenarlığı kaldırır
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32.0),
+                    ElevatedButton(
+                      onPressed: _onLoginButtonPressed,
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 28.0, horizontal: 48.0), // Butonun iç boşluğunu ayarlar
+                        textStyle: TextStyle(fontSize: 20.0), // Buton metninin yazı tipi boyutunu ayarlar
+                      ),
+                      child: Text('İlerle'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
