0da12f0361778670a0e891474941e21271685c1c
denizhanayinli <daydinli93@gmail.com>
2023-09-18 11:49:31 +0300

    test ekranına hamburger menü icon eklendi.bu iconun eklenmesi için appbar gerekiyordu.container yerine appbar içine alındı

--- lib/test.dart
+++ lib/test.dart
@@ -15,22 +15,30 @@ class _TestScreenState extends State<TestScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF302D2D),
-      body: Column(
-        children: [
-          Container(
-            color: widget.category.color, // Kategori rengine uygun olarak değiştirin
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: Center(
-              child: Text(
-                widget.category.name, // Kategori adını buraya ekleyin
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+      appBar: AppBar(
+        backgroundColor: widget.category.color,
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.menu), // Hamburger menü ikonu
+              onPressed: () {
+                // Hamburger menü işlevselliğini burada ekleyin
+              },
+            ),
+            Text(
+              widget.category.name, // Kategori adını buraya ekleyin
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
-          ),
+          ],
+        ),
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
           SizedBox(height: 16.0),
           Container(
             padding: EdgeInsets.symmetric(vertical: 16.0),
@@ -45,7 +53,9 @@ class _TestScreenState extends State<TestScreen> {
               ),
             ),
           ),
-          const Expanded(child: SizedBox(),
+          SizedBox(height: 16.0),
+          Center(
+            child: SizedBox(), // Ortalamak için boş bir SizedBox kullanabilirsiniz
           ),
         ],
       ),
